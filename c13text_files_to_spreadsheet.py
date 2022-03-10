cf33d194239c010f9553703fb26bdccb6546dc55
Evan Chu <49127693+readchu@users.noreply.github.com>
2022-03-10 08:52:44 -0500

    fixed, "final" version
    Added enumerate() to my loops

--- c13text_files_to_spreadsheet.py
+++ c13text_files_to_spreadsheet.py
@@ -14,27 +14,39 @@ logging.basicConfig(
 )
 logging.disable(logging.CRITICAL)
 
+#could write it instead to somehow ask user to specify a folder
+#and also to just specify certain files instead
+
 def lines_to_sheet():
     wb = openpyxl.Workbook()
     sheet = wb.active
 
     f = Path.cwd()
 
-    file_list = list(f.glob("*.txt"))
+    file_list = list(f.glob('*.txt'))
 
-    for file in file_list:
-        file_position = file_list.index(file) + 1
+    for fl_index, file in enumerate(file_list):
+        file_position = fl_index + 1
         sheet.cell(row=1, column=file_position).value = file.name
         open_file = open(file)
         file_lines = open_file.readlines()
-        for line in file_lines:
+        for line_index, line in enumerate(file_lines):
             sheet.cell(
-                row=file_lines.index(line) + 2, column=file_position
-            ).value = line
+                row=line_index + 2, column=file_position
+            ).value = line.strip()
+            logging.debug(f'This is the active line: {line}')
+            logging.debug(f'This is where it\'ll be placed: {sheet.cell(row=file_lines.index(line) + 2, column=file_position)}')
             #alternative value=text[line] instead of ().value = line
-    filename = f"Separated_lines_from_{f.name}.xlsx"
+            #use .strip() to reduce whitespace in sheet
+
+#    debug_column = []
+#    for row in sheet['C']:
+#        debug_column.append(row.value)
+#    logging.debug(f'This is the contents of column C: {debug_column}')
+
+    filename = f'Separated_lines_from_{f.name}.xlsx'
     try:
         wb.save(filename)
     except PermissionError:
-        print(f"Saving {filename} not permitted. Terminated...")
+        print(f'Saving {filename} not permitted. Terminated...')
         sys.exit()
